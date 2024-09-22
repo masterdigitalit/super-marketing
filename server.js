725cb0eb00422f94f6920da4db4df9473241e19f
Maksim Kozyrev <masterdigitalit@gmail.com>
2024-09-22 22:43:52 +0300

    morgan

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
+
+  
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
+app.enable("trust proxy");
+app.use(morgan("dev"))
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
 app.get('*', (req, res) => {
+    const ipAddress = req.socket.remoteAddress;
+    console.log(ipAddress)
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
